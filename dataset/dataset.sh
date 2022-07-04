f06e93d711cd8dc792d1680c4a9b8e455c30ee64
reshalfahsi <resha.alfahsi@gmail.com>
2022-07-04 22:57:22 +0700

    update bash dataset

--- dataset/dataset.sh
+++ dataset/dataset.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+pip3 install dtrx
 
 curl -LJO "https://github.com/hasibzunair/3D-image-classification-tutorial/releases/download/v0.2/CT-0.zip" --output "CT-0.zip"
 curl -LJO "https://github.com/hasibzunair/3D-image-classification-tutorial/releases/download/v0.2/CT-23.zip" --output "CT-23.zip"
 
-unzip "CT-0.zip" -d "./"
-unzip "CT-23.zip" -d "./"
+dtrx CT-0.zip
+dtrx CT-23.zip
 
 rm "CT-0.zip"
 rm "CT-23.zip"
